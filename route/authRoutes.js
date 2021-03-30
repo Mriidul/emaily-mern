d429f3490b7dd5aff35db43e2722d8892d662e9f
Your Name <you@example.com>
2021-03-30 20:46:33 +0530

    Finish AuthFlow2

--- route/authRoutes.js
+++ route/authRoutes.js
@@ -2,11 +2,10 @@ const passport = require("passport");
 
 module.exports = (app) => {
     app.get("/auth/google", passport.authenticate("google", {
-            scope: ['profile', 'email']
-        },
-        app.get("/auth/facebook", passport.authenticate("facebook"))));
+        scope: ['profile', 'email']
+    }, ));
 
-    app.get("/auth/facebook/callback", passport.authenticate("facebook"));
+
     app.get("/auth/google/callback", passport.authenticate("google"));
 
 
