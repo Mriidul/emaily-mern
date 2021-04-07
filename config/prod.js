06e79a45c6b7aebfd01a0f5d041f5e9118336fc2
Your Name <you@example.com>
2021-04-07 09:43:45 +0530

    Some minor changes and built billing etc

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 //Production keys here
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKeys: process.env.COOKIE_KEY,
